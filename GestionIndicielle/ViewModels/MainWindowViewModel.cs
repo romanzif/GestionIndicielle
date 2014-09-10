71f978a0e843411b9617ea461daa9e4ed89ea090
Leste Julien <julien.lestee@gmail.com>
2014-09-10 14:42:44 +0200

    Text Box budget periode

--- GestionIndicielle/ViewModels/MainWindowViewModel.cs
+++ GestionIndicielle/ViewModels/MainWindowViewModel.cs
@@ -1,20 +1,74 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Input;
 using GestionIndicielle.Models;
 using GestionIndicielle.Parser;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
+
 
 namespace GestionIndicielle.ViewModels
 {
-    public class MainWindowViewModel : Window
+    public class MainWindowViewModel : BindableBase
     {
         public Matrice D,I;
         public DateTime Tfin = new DateTime(2006, 1, 17, 0, 0, 0);
         public DateTime Tdebut = new DateTime(2006, 1, 2, 0, 0, 0);
-        //public DateTime Tfin { get; private set; }
-        //public DateTime Tdebut { get; private set; }
-         public MainWindowViewModel()
-        {
+        private string _periodeEstimation, _periodeRebalancement,_budget;
+        private int _perEstimation, _perRebalancement,_budg;
 
+
+        public string  PeriodeEstimation
+        {
+            get { return _periodeEstimation; }
+            set
+            {
+                if (_periodeEstimation != value)
+                {
+                    _periodeEstimation = value;
+                    OnPropertyChanged(() =>PeriodeEstimation);
+                    _perEstimation = int.Parse(_periodeEstimation);
+                };
+            }
+ 
+        }
+
+        public string PeriodeRebalancement
+        {
+            get { return _periodeRebalancement; }
+
+            set
+            {
+                if (_periodeRebalancement != value)
+                {
+                    _periodeRebalancement = value;
+                    OnPropertyChanged(()=>PeriodeRebalancement);
+                    _perRebalancement = int.Parse(_periodeRebalancement);
+                }
+            }
+        }
+
+        public string Budget
+        {
+            get { return _budget; }
+            set
+            {
+                if(_budget != value)
+                {
+                    _budget = value;
+                    OnPropertyChanged(() => Budget);
+                    _budg = int.Parse(_budget);
+                };
+            }
+
+        }
+        public ICommand Selection { get; private set; }
+
+       
+
+         public MainWindowViewModel()
+         {
+            Selection = new DelegateCommand(Click);
 
             D = new Matrice(DaysIgnoreWeekends(Tdebut,Tfin), 29);
             I = new Matrice(DaysIgnoreWeekends(Tdebut, Tfin), 1);
@@ -45,6 +99,30 @@ namespace GestionIndicielle.ViewModels
         {
             Tfin = d;
         }
-      
+
+        public void Click()
+        {
+            SelectBalancement();
+            SelectEstimation();
+            SelectBudget();
+        }
+
+        private void SelectBalancement()
+        {
+            //PeriodeRebalancement = Rebalancement.selectedText();
+            Console.Write("Balancement");
+        }
+
+        private void SelectEstimation()
+        {
+           // PeriodeEstimation = Estimation.selectedText();
+            Console.Write("Estimation");
+        }
+
+        private void SelectBudget()
+        {
+            // PeriodeEstimation = Estimation.selectedText();
+            Console.Write("Budget");
+        }
     }
 }
